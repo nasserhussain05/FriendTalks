d50748fe059b95ac3ec7083153f687bac19166f1
nasserhussain05 <97998771+nasserhussain05@users.noreply.github.com>
2022-04-15 11:29:13 +0530

    Update nodemailer.js

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -9,7 +9,7 @@ let transporter = nodemailer.createTransport({
     secure: false,
     auth: {
         user: 'photographyco123@gmail.com',
-        pass: 'qsrjokawwevwxuwp'
+        pass: '***********'
     }
 });
 
